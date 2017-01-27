35e16efaf061f68f10ca19d9c79f2fe3000f3403
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-01-27 23:02:13 +0200

    New test class: GameTest. Tests for Game.
    	new file:   HyperWeiqi/src/test/java/hyperweiqi/domain/GameTest.java

---
+++ HyperWeiqi/src/test/java/hyperweiqi/domain/GameTest.java
@@ -0,0 +1,28 @@
+package hyperweiqi.domain;
+
+import hyperweiqi.ai.AiPlayer;
+import hyperweiqi.logic.HumanPlayer;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    public GameTest() {
+    }
+
+    @Test
+    public void testGame() {
+        int boardSize = 3;
+
+        Player player1 = new AiPlayer("AI player");
+        Player player2 = new HumanPlayer("Human player");
+
+        Game game = new Game(boardSize, player1, player2);
+        assertEquals(game.getBoardSize(), boardSize);
+        assertEquals(game.getBoard().getSize(), boardSize);
+        assertEquals(game.getMoveIndex(), 0);
+        assertEquals(game.getKo(), null);
+        assertEquals(game.getPlayer1(), player1);
+        assertEquals(game.getPlayer2(), player2);
+    }
+}
