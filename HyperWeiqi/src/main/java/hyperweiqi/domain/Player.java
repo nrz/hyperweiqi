8150f73cde5adf4887614ba47edfff8f2d3dacd2
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-01-27 22:52:32 +0200

    Abstract class Player. Will be extended by HumanPlayer and AiPlayer.
    	new file:   HyperWeiqi/src/main/java/hyperweiqi/domain/Player.java

---
+++ HyperWeiqi/src/main/java/hyperweiqi/domain/Player.java
@@ -0,0 +1,32 @@
+package hyperweiqi.domain;
+
+import hyperweiqi.logic.Move;
+
+public abstract class Player {
+    
+    private String name;
+    private final Type type;
+    
+    public enum Type {
+        HUMAN, AI
+    }
+
+    public Player(String name, Type type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Type getType() {
+        return this.type;
+    }
+    
+    abstract public Move getMove();
+}
