a078a1094f2123b4f9c2ab8ba8aab668e73e36bf
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-01-27 18:24:41 +0200

    New domain class: `StoneLocation`.
    `StoneLocation` contains X and Y coordinates of a given board location.
    
    	new file:   HyperWeiqi/src/main/java/hyperweiqi/domain/StoneLocation.java

---
+++ HyperWeiqi/src/main/java/hyperweiqi/domain/StoneLocation.java
@@ -0,0 +1,20 @@
+package hyperweiqi.domain;
+
+public class StoneLocation {
+
+    private final int x;
+    private final int y;
+
+    public StoneLocation(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+}
