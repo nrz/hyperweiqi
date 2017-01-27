6e47728123745ca4a7fce4393785e052d9bd00a1
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-01-27 18:26:36 +0200

    New domain class: `Stone`.
    	new file:   HyperWeiqi/src/main/java/hyperweiqi/domain/Stone.java

---
+++ HyperWeiqi/src/main/java/hyperweiqi/domain/Stone.java
@@ -0,0 +1,43 @@
+package hyperweiqi.domain;
+
+public class Stone {
+
+    private final Color color;
+    private final StoneLocation location;
+    private boolean captured;
+
+    public enum Color {
+
+        BLACK, WHITE
+    }
+
+    public Stone(Color color, StoneLocation location) {
+        this.color = color;
+        this.location = location;
+        this.captured = false;
+    }
+
+    public Stone(Color color, int x, int y) {
+        this(color, new StoneLocation(x, y));
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public int getX() {
+        return this.location.getX();
+    }
+
+    public int getY() {
+        return this.location.getY();
+    }
+
+    public boolean isCaptured() {
+        return this.captured;
+    }
+
+    public void capture() {
+        this.captured = true;
+    }
+}
