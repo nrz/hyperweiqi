860b863cb4a8f90b19630ba9646e07d98e89ce12
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-01-27 18:27:41 +0200

    New domain class: `Board`.
    `Board` contains information about board size and stones on board.
    
    Captured stones, ko situations, etc. are not stored on a `Board` object.
    
    	new file:   HyperWeiqi/src/main/java/hyperweiqi/domain/Board.java

---
+++ HyperWeiqi/src/main/java/hyperweiqi/domain/Board.java
@@ -0,0 +1,57 @@
+package hyperweiqi.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Board {
+
+    private final int size;
+    private final Stone[][] board;
+
+    // Locations map is to provide access from stones to locations.
+    private final Map<Stone, StoneLocation> locations;
+
+    public Board(int size) {
+        this.size = size;
+        this.board = new Stone[size][size];
+        this.locations = new HashMap<>();
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public boolean placeStone(Stone stone, StoneLocation location) {
+        int x = location.getX();
+        int y = location.getY();
+
+        if (!checkCoordinates(x, y)) {
+            // Can not place a Stone outside the board!
+            return false;
+        }
+
+        if (stone == null) {
+            // Can not place a null stone!
+            return false;
+        }
+
+        this.board[y][x] = stone;
+        this.locations.put(stone, location);
+        return true;
+    }
+
+    public boolean placeStone(Stone stone, int x, int y) {
+        return this.placeStone(stone, new StoneLocation(x, y));
+    }
+
+    public Stone at(int x, int y) {
+        if (!this.checkCoordinates(x, y)) {
+            return null;
+        }
+        return this.board[y][x];
+    }
+
+    private boolean checkCoordinates(int x, int y) {
+        return (x >= 0 && x < this.size && y >= 0 && y < this.size);
+    }
+}
