d6115895b2fa15e8550193ebf4d349c70165e990
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-02-04 09:58:43 +0200

    New domain class: `Sgf`. `Sgf` is for processing .sgf files.
    	new file:   HyperWeiqi/src/main/java/hyperweiqi/domain/Sgf.java

---
+++ HyperWeiqi/src/main/java/hyperweiqi/domain/Sgf.java
@@ -0,0 +1,51 @@
+package hyperweiqi.domain;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Sgf {
+
+    private String filename;
+    private File file;
+    private List<String> content;
+
+    public Sgf(String filename) throws FileNotFoundException {
+        this.filename = filename;
+        this.file = new File(this.filename);
+        this.content = new ArrayList<>();
+
+        if (this.file.isFile()) {
+            Scanner scanner = new Scanner(this.file);
+            while (scanner.hasNextLine()) {
+                this.content.add(scanner.nextLine());
+            }
+        }
+    }
+
+    public String getFilename() {
+        return this.filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public File getFile() {
+        return this.file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public List<String> getContent() {
+        return this.content;
+    }
+
+    public void setContent(List<String> content) {
+        this.content = content;
+    }
+}
