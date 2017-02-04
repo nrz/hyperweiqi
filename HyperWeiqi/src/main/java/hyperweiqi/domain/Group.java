2e795a3c75bd2a44a1e59a5e0892d610e49c2361
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-02-04 09:57:06 +0200

    New class: `Group`. `Group` is a group of `Stone` Objects.
    	new file:   HyperWeiqi/src/main/java/hyperweiqi/domain/Group.java

---
+++ HyperWeiqi/src/main/java/hyperweiqi/domain/Group.java
@@ -0,0 +1,20 @@
+package hyperweiqi.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Group {
+
+    // Group is a group of stones connected with horizontally and/or vertically.
+
+    private final Map<StoneLocation, Stone> stones;
+
+    public Group() {
+        this.stones = new HashMap<>();
+    }
+
+    public boolean alive() {
+        // TODO: implement this using breadth-first search!
+        return false;
+    }
+}
