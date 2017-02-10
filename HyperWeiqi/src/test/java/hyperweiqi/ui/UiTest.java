97136c9770710d2ff35c09c94f700ed8537054d8
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-02-10 23:19:42 +0200

    Test class for abstract class `Ui`.
    	new file:   HyperWeiqi/src/test/java/hyperweiqi/ui/UiTest.java

---
+++ HyperWeiqi/src/test/java/hyperweiqi/ui/UiTest.java
@@ -0,0 +1,38 @@
+package hyperweiqi.ui;
+
+import hyperweiqi.domain.Player;
+import hyperweiqi.domain.StoneLocation;
+import hyperweiqi.logic.Logic;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UiTest {
+
+    public UiTest() {
+        Logic logic = new Logic();
+        Ui ui = new Ui(logic) {
+
+            @Override
+            public int getBoardSize() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public Player.Type getPlayerType() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public StoneLocation getMove() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        };
+
+        Assert.assertEquals(ui.getLogic(), logic);
+    }
+
+    @Test
+    public void testStart() {
+    }
+
+}
